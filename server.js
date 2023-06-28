2148ba00627d40c90184ac90c18b84a92c4ae221
Jan <pcerservices@gmail.com>
2023-06-28 18:32:50 +0200

    Getting data for API for specific city and showing the temperature, location and weather status and icon

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+const { log } = require("console");
 const express = require("express");
+//Using HTTPS node module: https://nodejs.org/api/https.html#httpsgeturl-options-callback
+const https = require("https");
 
 const app = express();
 
@@ -7,5 +10,29 @@ app.listen(3000, function() {
 })
 
 app.get("/", function(req, res) {
-    res.send("Server running");
+
+    const url = "https://api.openweathermap.org/data/2.5/weather?lat=-33.91&lon=25.57&units=metric&APPID=ff3c37aef04eda84d0b474abb2bf615f";
+
+    https.get(url, function(response) {        
+
+        response.on("data", function(data) {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const description = weatherData.weather[0].description;
+            const location = weatherData.name;
+            const icon = weatherData.weather[0].icon;
+            const iconUrl = "https://openweathermap.org/img/wn/";
+            const iconExt = "@2x.png"
+            const fullIconUrl = iconUrl + icon + iconExt;
+            console.log(fullIconUrl);
+
+            res.write(`<h1>The temperature in ${location} is ${temp} degrees celsius</h1>`)
+            res.write(`<h2>Weather status: ${description}</h2>`)
+            res.write(`<img src=${fullIconUrl} />`)
+            res.send();            
+        })
+    })    
 })
+
+
+
